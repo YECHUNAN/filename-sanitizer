c736b009a1e3df7c4dd0b23cc4b3eccce18743c6
Chunan Ye <yechunan@umich.edu>
2019-12-29 16:04:09 -0800

    Support sanitizing whole path on each segment

--- src/Sanitizer.ts
+++ src/Sanitizer.ts
@@ -1,4 +1,5 @@
 import * as os from "os";
+import * as path from "path";
 
 type SanitizePair = { origin: string, replacement: string };
 
@@ -39,6 +40,19 @@ export class Sanitizer {
         }
     }
 
+    public static sanitizePath(pathName: string): string {
+        if (os.platform() === "win32") {
+            // Windows
+            return this.sanitizePathWindows(pathName);
+        } else if (os.platform() === "darwin") {
+            // macOS
+            return this.sanitizePathDarwin(pathName);
+        } else {
+            // Linux
+            return this.sanitizePathLinux(pathName);
+        }
+    }
+
     public static sanitizeWindows(filename: string): string {
         windowsReplacements.forEach((pair) => {
             filename = filename.split(pair.origin).join(pair.replacement);
@@ -62,6 +76,43 @@ export class Sanitizer {
             filename = filename.split(pair.origin).join(pair.replacement);
         });
         return filename;
+    }
+
+    public static sanitizePathWindows(pathName: string) {
+        let pathSegments: string[];
+        let UNCPrefix = pathName.startsWith("\\\\?\\") ? "\\\\?\\" : "";
+        // handle UNC path, which start with \\?\
+        if (!!UNCPrefix) {
+            pathSegments = pathName.slice(4).split(path.sep);
+        } else {
+            pathSegments = pathName.split(path.sep);
+        }
+        // handle the disk part in path (e.g. C:)
+        pathSegments = pathSegments.map((segmentName, index) => {
+            if (index === 0 && /[a-zA-Z]:/.test(segmentName)) {
+                return segmentName;
+            }
+            return this.sanitize(segmentName);
+        });
+        return UNCPrefix + pathSegments.join(path.sep);
+    }
+
+    public static sanitizePathDarwin(pathName: string) {
+        let pathSegments: string[];
+        pathSegments = pathName.split(path.sep);
+        pathSegments = pathSegments.map((segmentName) => {
+            return this.sanitize(segmentName);
+        });
+        return pathSegments.join(path.sep);
+    }
+
+    public static sanitizePathLinux(pathName: string) {
+        let pathSegments: string[];
+        pathSegments = pathName.split(path.sep);
+        pathSegments = pathSegments.map((segmentName) => {
+            return this.sanitize(segmentName);
+        });
+        return pathSegments.join(path.sep);
     }
 }
 
